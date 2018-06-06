71eb4ed0c84a4701de1ab455345596d1d06c4fd0
miguelpeixe <miguel@cardume.art.br>
2018-06-06 12:43:23 -0300

    update heading font

--- functions.php
+++ functions.php
@@ -2,6 +2,6 @@
 
 function update_theme_enqueue_styles() {
   wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css' );
-  wp_enqueue_style('webfont-play', 'https://fonts.googleapis.com/css?family=Play:400,700');
+  wp_enqueue_style('webfont-play', 'https://fonts.googleapis.com/css?family=Playfair+Display:700,900"');
 }
 add_action( 'wp_enqueue_scripts', 'update_theme_enqueue_styles' );
