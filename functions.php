e90b4925da3e6274fa2f30cefab224a3997fa8ac
Miguel Peixe <miguel@cardume.art.br>
2018-09-17 12:22:11 -0300

    update styles, add sidebar header widgets and floating newsletter form

--- functions.php
+++ functions.php
@@ -1,7 +1,50 @@
 <?php
 
-function update_theme_enqueue_styles() {
+function update_theme_enqueue_scripts() {
   wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css' );
   wp_enqueue_style('webfont-play', 'https://fonts.googleapis.com/css?family=Playfair+Display:700,900"');
+
+  wp_enqueue_script('update-site', get_stylesheet_directory_uri() . "/js/site.js");
 }
-add_action( 'wp_enqueue_scripts', 'update_theme_enqueue_styles' );
+add_action( 'wp_enqueue_scripts', 'update_theme_enqueue_scripts' );
+
+function update_header_widgets_init() {
+
+	register_sidebar( array(
+		'name'          => 'Header sidebar',
+		'id'            => 'header_sidebar_1',
+		'before_widget' => '<div id="%1$s" class="widget-item %2$s">',
+		'after_widget'  => '</div>',
+		'before_title'  => '<h2>',
+		'after_title'   => '</h2>',
+	) );
+
+}
+add_action( 'widgets_init', 'update_header_widgets_init' );
+
+function update_header_top() {
+  if ( is_active_sidebar( 'header_sidebar_1' ) ) :
+    ?>
+  	<div id="header-sidebar" class="header-sidebar widget-area" role="complementary">
+  		<?php dynamic_sidebar( 'header_sidebar_1' ); ?>
+  	</div><!-- #header-sidebar -->
+    <?php
+  endif;
+}
+add_action("et_header_top", "update_header_top");
+
+function update_newsletter_form() {
+  $form = do_shortcode( '[mc4wp_form] ');
+  if($form) :
+    ?>
+    <div id="update-newsletter-form" class="update-newsletter-form-container">
+      <div class="update-newsletter-form">
+        <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/news_bg_.png" class="bg-img" />
+        <h2>Receba nossa newsletter sobre inovação política</h2>
+        <?php echo do_shortcode( '[mc4wp_form]' ); ?>
+      </div>
+    </div>
+    <?php
+  endif;
+}
+add_action( 'wp_footer', 'update_newsletter_form' );
---
+++ js/site.js
@@ -0,0 +1,52 @@
+(function($) {
+  /* Newsletter */
+  function setNewsletterCookie() {
+    var d = new Date();
+    var days = 10;
+    d.setTime(d.getTime + days * 24 * 60 * 60 * 1000);
+    var expires = "expires=" + d.toUTCString();
+    document.cookie = "sentNewsletter=true;" + expires + ";path=/";
+  }
+  function getNewsletterCookie() {
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(";");
+    for (var i = 0; i < ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == " ") {
+        c = c.substring(1);
+      }
+      if (c.indexOf("sentNewsletter=") == 0) {
+        return c.substring("sentNewsletter=", c.length);
+      }
+    }
+  }
+  $(document).ready(function() {
+    $("#update-newsletter-form").each(function() {
+      var $node = $(this);
+      $("body").css({
+        paddingBottom: "130px"
+      });
+      var $close = $(
+        "<a href='javascript:void(0);' class='close-button'>x</a>"
+      );
+      $node.find(".update-newsletter-form").append($close);
+      if (getNewsletterCookie()) {
+        $node.addClass("blocked");
+        $("body").css({
+          paddingBottom: 0
+        });
+      }
+      $close.on("click", function(ev) {
+        ev.preventDefault();
+        $node.addClass("blocked");
+        $("body").css({
+          paddingBottom: 0
+        });
+      });
+      $node.find("form").on("submit", function() {
+        setNewsletterCookie();
+        // $node.addClass("hidden");
+      });
+    });
+  });
+})(jQuery);
